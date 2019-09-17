f3f8013897713167049478e3a8e6186b43fdb3be
Mario <luix30lp@gmail.com>
2019-09-16 23:29:21 -0600

    ups

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -1,9 +1,5 @@
 export default {
-	apiKey: 'AIzaSyCZRm71E2o7pXlh8SMROI9SyUj_1pmqtSA',
-	authDomain: 'itqin-a4449.firebaseapp.com',
-	databaseURL: 'https://itqin-a4449.firebaseio.com',
-	projectId: 'itqin-a4449',
-	storageBucket: 'itqin-a4449.appspot.com',
-	messagingSenderId: '557433159782',
-	appId: '1:557433159782:web:d6f7798819140c58'
+	/**
+	 * Aqui va la informacion con el API key para firebase
+	 */
 };
